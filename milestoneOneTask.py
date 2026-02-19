e8b477419ff39416feb79a8091acc5bef1d4b227
Harry C <34387466+Hazzer890@users.noreply.github.com>
2026-02-19 09:44:58 +0800

    Refactor positions and update movement logic
    Changes made in lab, with robot testing and with milestone constraints

--- milestoneOneTask.py
+++ milestoneOneTask.py
@@ -1,43 +1,50 @@
 from xarmclient import XArm
 from enum import Enum
-from time import sleep
+import time
 
 # Variables #
 tolerance = 1.1
 
-class position(Enum):
-    # (YAW, SHOULDER, ELBOW, WRIST_ROTATE, WRIST_BWK_ROT, WRIST_FWD_ROT)
-    READIED = (0,0,0,0,0,0)
-    HOVER_LEFT = (0,0,0,0,0,0)
-    LEFT = (0,0,0,0,0,0)
-    HOVER_RIGHT = (0,0,0,0,0,0)
-    RIGHT = (0,0,0,0,0,0)
+# (YAW, SHOULDER, ELBOW, WRIST_ROTATE, WRIST_BWK_ROT, WRIST_FWD_ROT)
+READIED = (0,28,-56,-1,-56,4)
+HOVER_LEFT = (-55,0,-34,3,-53,-61)
+HIGH_LEFT = (-55,0,0,3,-53,-61)
+LEFT = (-56,-7,-35,3,-44.5,-64)
+HOVER_RIGHT = (55,-5,-27,-1,-56,57)
+HIGH_RIGHT = (55,-5,0,-1,-56,57)
+RIGHT = (55,-10,-30,-2,-50,55)
 
 # Functions #
 def reachedPosition(target_joints, current_joints):
     if all(abs(current - target) <= tolerance
            for current, target in zip(current_joints, target_joints)):
         return True
-    else
+    else:
         return False
 
 def attemptMovement(target):
+    xarm.set_joints(target)
     while not reachedPosition(target, xarm.get_joints()):
-        xarm.set_joints(target)
+        pass
 
 # Script #
 print("Begin")
+xarm = XArm()
 xarm.home()
 xarm.grip(0)
 time.sleep(1)
-attemptMovement(position.READIED)
-attemptMovement(position.HOVER_LEFT)
-attemptMovement(position.LEFT)
+attemptMovement(READIED)
+attemptMovement(HIGH_LEFT)
+attemptMovement(HOVER_LEFT)
+attemptMovement(LEFT)
 xarm.grip(1)
 time.sleep(1)
-attemptMovement(position.HOVER_RIGHT)
-attemptMovement(position.RIGHT)
+attemptMovement(HIGH_LEFT)
+attemptMovement(HIGH_RIGHT)
+attemptMovement(HOVER_RIGHT)
+attemptMovement(RIGHT)
 xarm.grip(0)
-attemptMovement(position.HOVER_RIGHT
-attemptMovement(position.READIED)
+attemptMovement(HOVER_RIGHT)
+attemptMovement(READIED)
+xarm.rest()
 print("End")
