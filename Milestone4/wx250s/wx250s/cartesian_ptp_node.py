082c856aac96cc88c9e714448eed6aa4f7fa8fb8
Harry C <34387466+Hazzer890@users.noreply.github.com>
2026-04-30 10:49:27 +0800

    Milestone 6 Cont.
    6.2 stable, no 6.3

--- Milestone4/wx250s/wx250s/cartesian_ptp_node.py
+++ Milestone4/wx250s/wx250s/cartesian_ptp_node.py
@@ -26,6 +26,8 @@ class MinimalActionServer(Node):
         self.goal_handle = None
         self.goal_lock = threading.Lock()
         self.xarm = XArm()
+        self.intermediate_targets = []
+
         # Action servers are created using interface type, action name and multiple callback functions
         self.action_server = ActionServer(
             self,
@@ -41,30 +43,25 @@ class MinimalActionServer(Node):
         self.action_server.destroy()
         super().destroy_node()
 
-    def inverse_kinematics(self, goal_request):
-        self.get_logger().info(f"inverse kinematics = cool")
+    def inverse_kinematics(self, x, y, z, current_joints):
 
-        current_joints = self.xarm.get_joints()
         roll = 0
         pitch = 0
         yaw = 0
 
-        translation = np.array((goal_request.x, goal_request.y, goal_request.z))
+        translation = np.array((x, y, z))
 
-        self.get_logger().info(f"inverse kinematics = dumb")
         rot = Rotation.from_euler('XYZ', [roll, pitch, yaw], degrees=True)
-        self.get_logger().info(f"inverse kinematics = dumber")
         rotation_matrix = rot.as_matrix()
-        self.get_logger().info(f"inverse kinematics = dumbest")
-
-        htm = np.hstack(rotation_matrix, translation.reshape(3,1)) ## I am the problem
-        self.get_logger().info(f"inverse kinematics = dumbe!!")
+        
+        htm = np.zeros((3, 4))
+        htm[:3, :3] = rotation_matrix
+        htm[:3, -1] = translation
 
         joints = wx250s_kinematics.ik(current_joints, htm)
         
-        if joints == None:
+        if joints is None:
             self.get_logger().info(f"Did Not Converge")
-        self.get_logger().info(f"Did Converge")
 
         return joints
     
@@ -83,22 +80,29 @@ class MinimalActionServer(Node):
     def goal_callback(self, goal_request):
         self.get_logger().info(f"goal set")
 
-        joint_goal = self.inverse_kinematics(goal_request)
-        # Accept or reject a client request to begin an action.
-        if self.xarm.is_goal_valid(joint_goal) == 0: ## Fix
-            self.get_logger().info("Received valid goal request")
-            return GoalResponse.ACCEPT
+        self.calculate_intermediate_targets(goal_request.x, goal_request.y, goal_request.z)
 
-        self.get_logger().info("Received invalid goal request")
-        return GoalResponse.REJECT
+        current_joints = self.xarm.get_joints()
+
+        for target in self.intermediate_targets:
+            joint_goal = self.inverse_kinematics(target[0], target[1], target[2], current_joints)
+            # Accept or reject a client request to begin an action.
+            if self.xarm.is_goal_valid(joint_goal) != 0: 
+
+                self.get_logger().info("Received invalid goal request")
+                return GoalResponse.REJECT
+
+            current_joints = joint_goal
+        self.get_logger().info("Received valid goal request")
         # ... or return GoalResponse.REJECT if goal should be rejected
+        return GoalResponse.ACCEPT
 
     # This function is called whenever new goal has been accepted
     def handle_accepted_callback(self, goal_handle):
         with self.goal_lock:
             # This server only allows one goal at a time
             if self.goal_handle is not None and self.goal_handle.is_active:
-                self.xarm.set_joints(self.xarm.get_joints()) ## Fix
+                self.xarm.set_joints(self.xarm.get_joints(), "high_acc") ## Fix
 
                 self.get_logger().info("Aborting previous goal")
                 # Abort the existing goal
@@ -111,56 +115,92 @@ class MinimalActionServer(Node):
     def cancel_callback(self, goal):
         # Accept or reject a client request to cancel an action.
         self.get_logger().info("Received cancel request")
-        self.xarm.set_joints(self.xarm.get_joints())
+        self.xarm.set_joints(self.xarm.get_joints(), "high_acc")
 
         return CancelResponse.ACCEPT
+
+    def calculate_intermediate_targets(self, x, y, z):
+        self.intermediate_targets = []
+        max_step_size = 30 #mm
+
+        current_position = self.forward_kinematics()
+        dx = x - current_position[0]
+        dy = y - current_position[1]
+        dz = z - current_position[2]
+
+        distance = np.square(dx) + np.square(dy) + np.square(dz)
+        distance = np.sqrt(distance)
+
+        steps_to_take = np.ceil(distance / max_step_size)
+        step_size = distance / steps_to_take
+
+        for step in range(round(steps_to_take)):
+            new_x = current_position[0] + dx / steps_to_take * (step + 1) # Doesn't include current pos, does includefinal pos
+            new_y = current_position[1] + dy / steps_to_take * (step + 1) # Doesn't include current pos, does includefinal pos
+            new_z = current_position[2] + dz / steps_to_take * (step + 1) # Doesn't include current pos, does includefinal pos
+            
+
+            self.intermediate_targets.append([new_x, new_y, new_z])
 
     # This function is called at the start of action execution
     def execute_callback(self, goal_handle):
         self.get_logger().info("Executing goal...")
 
-        target =  self.inverse_kinematics(goal_handle.request)
+ 
+
+        current_step = 0
 
         result = CartesianPTP.Result()
 
         feedback_msg = CartesianPTP.Feedback()
-        self.xarm.set_joints(target)
-        current_value = self.xarm.get_joints()
-        prev_value = current_value
-        time.sleep(0.2)
+        current_joints = self.xarm.get_joints()
+        prev_value = current_joints
 
-        while True:
-            current_value = self.xarm.get_joints()
-            if not goal_handle.is_active:
-                return CartesianPTP.Result()
+        tolerance_degrees = 2
 
-            if goal_handle.is_cancel_requested:
-                goal_handle.canceled()
-                return CartesianPTP.Result()
-
-            if all(abs(current - target) <= 5
-                   for current, target in zip(current_value, target)):
-                break
+        for current_target in self.intermediate_targets:
             
-            current_position = self.forward_kinematics()
-            feedback_msg.x_current = current_position[0]
-            feedback_msg.y_current = current_position[1]
-            feedback_msg.z_current = current_position[2]
 
-            goal_handle.publish_feedback(feedback_msg)
+            current_joints = self.xarm.get_joints()
+            current_target_joints = self.inverse_kinematics(current_target[0], current_target[1], current_target[2], self.xarm.get_joints())
+            self.xarm.set_joints(current_target_joints, "high_acc")
 
-            for i in range(len(current_value)):
-                if abs(current_value[i] - prev_value[i]) < 0.5 and abs(current_value[i] - target[i]) > 5:
-                    self.get_logger().info("Stalled out")
-                    self.xarm.set_joints(self.xarm.get_joints())
-
-                    goal_handle.abort()
-                    
-
+            while not all(abs(current - target) <= tolerance_degrees
+                   for current, target in zip(current_joints, current_target_joints)):
+                if not goal_handle.is_active:
                     return CartesianPTP.Result()
 
-            prev_value = current_value
-            time.sleep(0.2)
+                if goal_handle.is_cancel_requested:
+                    goal_handle.canceled()
+                    return CartesianPTP.Result()
+
+                
+                
+                current_position = self.forward_kinematics()
+                feedback_msg.x_current = current_position[0] - self.intermediate_targets[-1][0]
+                feedback_msg.y_current = current_position[1] - self.intermediate_targets[-1][1]
+                feedback_msg.z_current = current_position[2] - self.intermediate_targets[-1][2]
+
+                goal_handle.publish_feedback(feedback_msg)
+
+
+
+                # for i in range(len(current_joints)):
+                #     if abs(current_joints[i] - prev_value[i]) < 0.5 and abs(current_joints[i] - current_target_joints[i]) > tolerance_degrees:
+                #         self.get_logger().info("Stalled out")
+                #         self.get_logger().info(f"current joints - {current_joints[i]}")
+                #         self.get_logger().info(f"prev joints - {prev_value[i]}")
+                #         self.get_logger().info(f"target joints - {current_target_joints[i]}")
+                #         self.xarm.set_joints(self.xarm.get_joints())
+
+                #         goal_handle.abort()
+                        
+
+                #         return CartesianPTP.Result()
+
+                prev_value = current_joints
+                current_joints = self.xarm.get_joints()
+                time.sleep(0.02)
 
         with self.goal_lock:
             if not goal_handle.is_active:
